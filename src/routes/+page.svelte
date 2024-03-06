dc94dac7ea38776a5cebafc6114e0083690d2995
tarsisius <tarsisiushp@gmail.com>
2024-03-06 22:23:10 +0700

    fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
+<script lang="ts">
+  import type { PageServerData } from "./$types"
+
+  export let data: PageServerData
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+
+{#each data.meta as meta}
+    <p>{meta.title}</p>
+  {/each}
