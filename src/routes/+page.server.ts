3c248442e7787727b69379ce10772291e2c9e4ba
tarsisius <tarsisiushp@gmail.com>
2024-03-07 08:10:42 +0700

    fix

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,16 +3,7 @@ import type { PageServerLoad } from "./$types"
 
 export const load: PageServerLoad = async () => {
   const meta = getAllMeta()
-
   return {
-    meta: [
-      {
-        title: 'Hello worldd',
-        slug: 'hello-world',
-        published: { date: '2024-03-03', time: '20:12' },
-        updated: { date: '2024-03-03', time: '20:12' }   
-      }
-    ]
-    ,
+    meta,
   }
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,15 @@
 <script lang="ts">
-  import type { PageServerData } from "./$types"
+  // import type { PageServerData } from "./$types"
 
-  export let data: PageServerData
+  // export let data: PageServerData
+  const metas = [
+      {
+        title: 'Hello worldd',
+        slug: 'hello-world',
+        published: { date: '2024-03-03', time: '20:12' },
+        updated: { date: '2024-03-03', time: '20:12' }   
+      }
+    ]
 </script>
 
 <h1>Welcome to SvelteKit</h1>
@@ -9,6 +17,6 @@
   Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
 </p>
 
-{#each data.meta as meta}
+{#each metas as meta}
   <p>{meta.title}</p>
 {/each}
