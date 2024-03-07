363b268837c555dc1f28b8fbdfa40d4abe18cf7a
tarsisius <tarsisiushp@gmail.com>
2024-03-07 08:13:52 +0700

    fix

--- src/routes/+page.server.ts
+++
@@ -1,9 +0,0 @@
-import { getAllMeta } from "$lib/extract"
-import type { PageServerLoad } from "./$types"
-
-export const load: PageServerLoad = async () => {
-  const meta = getAllMeta()
-  return {
-    meta,
-  }
-}
